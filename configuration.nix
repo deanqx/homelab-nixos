9810ff4da3690cf4a1e4f4474561c92980faf59b
deanqx <dean@kowatsch.de>
2026-03-14 10:39:07 +0100

    feat: add dean-home-windows ssh key

--- configuration.nix
+++ configuration.nix
@@ -159,6 +159,7 @@
     extraGroups = [ "wheel" ]; # Enable `sudo` for the user.
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID+E5ey8cjpUlHALMBFbDy9ijCd0M+w0iz0VIIE5cM77 dean-home"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIS1EgZ67VX7KNZ1IOCAwVFfZrLZLdEHlG6rGVoSJUiz dean-home-windows"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAmMvFppJL6njZ45WthZ3kM1Aq7bdPjbp+IsHUapOMm deanqx-pad"
     ];
     shell = pkgs.zsh;
