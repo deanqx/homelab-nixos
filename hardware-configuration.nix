f28bb6459b8185a23c452830282a82d4d896e67a
deanqx <dean@kowatsch.de>
2026-05-12 22:35:00 +0200

    feat: disable swap for stability

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -21,9 +21,8 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/f0fa0f25-6297-4e5a-9a0b-2e0fa3e89eed"; }
-    ];
+  swapDevices = lib.mkForce [ ];
+  #swapDevices = [ { device = "/dev/disk/by-uuid/f0fa0f25-6297-4e5a-9a0b-2e0fa3e89eed"; } ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
